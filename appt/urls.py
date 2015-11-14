69e0751f99a2530cd8469a4a1ea9db29c3d09d1f
Chris Barnes <chrisbarnes@Chriss-MacBook-Pro-2.local>
2015-11-14 04:38:18 -0800

    fixed urls

--- appt/urls.py
+++ appt/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 	url(r'^register/$', views.register, name = 'register'),
 	url(r'^login/$', views.user_login, name = 'login'),
 	url(r'^logout/$', views.user_logout, name ='logout'),
+	url(r'^(?P<pk>[0-9]+)/$', views.new_appointment, name='new_appointment'),
 ]
