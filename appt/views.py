97c32b89f11a3f8d15812fe09d4d845fc2466cb8
Chris Barnes <chrisbarnes@Chriss-MacBook-Pro-2.local>
2015-11-14 06:10:23 -0800

    fuck

--- appt/views.py
+++ appt/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response, get_object_or_404
+from django.shortcuts import render, render_to_response, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 
