eccf1fdf6b773ccf0ae2b669bd5612ad86fd6c22
Chris Barnes <chrisbarnes@Chriss-MacBook-Pro-2.local>
2015-11-14 00:08:16 -0800

    new views

--- appt/views.py
+++ appt/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Appointment
+
 # Create your views here.
 
 
@@ -10,4 +12,25 @@ def index(request):
 	return HttpResponse(output)
 
 
-	
+def monthly_view(request, month):
+	p = Appointment.objects.filter_by(date.month = month)
+
+
+	return HttpResponse('hello')
+
+
+def date_view(request,date):
+	return HttpResponse('hello')
+
+
+def new_appointment(request,date):
+	return HttpResponse('dicks')
+
+def confirmation(request,pk):
+	return HttpResponse('fuck you')
+
+def edit(request,pk):
+	return HttpResponse('fuck you')
+
+
+
