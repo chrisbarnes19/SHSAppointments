887d370d65feec7116e5bb187439d905a10c3a94
Chris Barnes <chrisbarnes@Chriss-MacBook-Pro-2.local>
2015-11-14 06:14:52 -0800

    fixes

--- appt/views.py
+++ appt/views.py
@@ -27,7 +27,7 @@ def monthly_view(request, month):
 @login_required
 def date_view(request,day,month,year):
 	
-	appointments = get_list_or_404(Appointment, date >= datetime.datetime(year = year, month = month, day = day), date <= datetime.datetime(year = year, month = month, day = day + 1), available = True)
+	appointments = get_list_or_404(Appointment, date__gte=datetime.datetime(year = year, month = month, day = day), date__lte = datetime.datetime(year = year, month = month, day = day + 1), available = True)
 
 	return render_to_response('appt/dateview.html',{'appointments':appointments,},context)
 
